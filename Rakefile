7203a9bd36f954d61d848bb4ec8cc2b4f35ea126
Stefan Penner <stefan.penner@gmail.com>
2009-02-12 21:56:13 -0600

    added test suite, stil hackish but its getting there

--- Rakefile
+++ Rakefile
@@ -1,10 +1,10 @@
-require 'rake'
-require 'rake/testtask'
-require 'rake/rdoctask'
-require 'rcov/rcovtask'
+require "rake"
+require "rake/testtask"
+require "rake/rdoctask"
+require "rcov/rcovtask"
 
 begin
-  require 'jeweler'
+  require "jeweler"
   Jeweler::Tasks.new do |s|
     s.name = "my_scaffold"
     s.summary = %Q{TODO}
@@ -18,23 +18,49 @@ rescue LoadError
 end
 
 Rake::TestTask.new do |t|
-  t.libs << 'lib'
-  t.pattern = 'test/**/*_test.rb'
+  t.libs << "lib"
+  t.pattern = "test/**/*_test.rb"
   t.verbose = false
 end
 
 Rake::RDocTask.new do |rdoc|
-  rdoc.rdoc_dir = 'rdoc'
-  rdoc.title    = 'my_scaffold'
-  rdoc.options << '--line-numbers' << '--inline-source'
-  rdoc.rdoc_files.include('README*')
-  rdoc.rdoc_files.include('lib/**/*.rb')
+  rdoc.rdoc_dir = "rdoc"
+  rdoc.title    = "my_scaffold"
+  rdoc.options << "--line-numbers" << "--inline-source"
+  rdoc.rdoc_files.include("README*")
+  rdoc.rdoc_files.include("lib/**/*.rb")
 end
 
 Rcov::RcovTask.new do |t|
-  t.libs << 'test'
-  t.test_files = FileList['test/**/*_test.rb']
+  t.libs << "test"
+  t.test_files = FileList["test/**/*_test.rb"]
   t.verbose = true
 end
 
-task :default => :rcov
+namespace :generator do
+  desc "Cleans up the test app before running the generator"
+  task :cleanup do
+    puts "removing test rails app"
+	system "sudo gem uninstall my_scaffold"
+    system "rm -rf test/rails_test"
+  end
+
+  desc "Run the generator on the tests"
+  task :generate do
+    puts "building gem"
+    system "rake gem"
+	system "rake install"
+    puts "generating rails app"
+	FileUtils.cd("test")
+	system "rails rails_test"
+	puts "swapping environment.rb file"
+	FileUtils.cp("environment.rb","rails_test/config/environment.rb")
+	FileUtils.cd("rails_test")
+    system "rake gems:unpack"
+	puts "./script/generate my_scaffold apple name:string"
+	system "./script/generate my_scaffold apple name:string"
+    system "rake db:migrate"
+ 	system "rake"
+  end
+end
+
--- generators/my_scaffold/my_scaffold_generator.rb
+++ generators/my_scaffold/my_scaffold_generator.rb
@@ -1,29 +1,8 @@
-#--
-# ShouldaScaffoldGeneratorConfig based on rubygems code.
-# Thank you Chad Fowler, Rich Kilmer, Jim Weirich and others.
-#++
-class MyScaffoldGeneratorConfig
-  
-  DEFAULT_TEMPLATING = 'haml'
-  DEFAULT_FUNCTIONAL_TEST_STYLE = 'basic'
-  
-  def initialize()
-    @config = load_file(config_file)
-    
-    @templating = @config[:templating] || DEFAULT_TEMPLATING
-    @functional_test_style = @config[:functional_test_style] || DEFAULT_FUNCTIONAL_TEST_STYLE
-  end
-  
-  attr_reader :templating, 
-              :functional_test_style
-   
-end
-
 class MyScaffoldGenerator < Rails::Generator::NamedBase
   default_options :skip_timestamps => false, 
                   :skip_migration  => false, 
 				  :skip_layout     => true
-
+ 
   attr_reader   :controller_name,
                 :controller_class_path,
                 :controller_file_path,
@@ -33,18 +12,21 @@ class MyScaffoldGenerator < Rails::Generator::NamedBase
                 :controller_underscore_name,
                 :controller_singular_name,
                 :controller_plural_name
-
+ 
   alias_method  :controller_file_name,  
-                :controller_underscore_name,
-                :controller_table_name, 
-				:controller_plural_name
+                :controller_underscore_name
 
+  alias_method  :controller_table_name, 
+                :controller_plural_name
+ 
   def initialize(runtime_args, runtime_options = {})
     super
     
-    @configuration   = MyScaffoldGeneratorConfig.new
+	options[:templating] = 'haml'
+	options[:functional_test_style] = 'basic'
+    
     @controller_name = @name.pluralize
-
+ 
     base_name, 
 	@controller_class_path, 
 	@controller_file_path, 
@@ -53,26 +35,27 @@ class MyScaffoldGenerator < Rails::Generator::NamedBase
     
 	@controller_class_name_without_nesting, 
 	@controller_underscore_name, 
-	@controller_plural_name   = inflect_names(base_name)
+	@controller_plural_name = inflect_names(base_name)
     
 	@controller_singular_name = base_name.singularize
-
+ 
     if @controller_class_nesting.empty?
-      @controller_class_name  = @controller_class_name_without_nesting
+      @controller_class_name = @controller_class_name_without_nesting
     else
-      @controller_class_name  = "#{@controller_class_nesting}::#{@controller_class_name_without_nesting}"
+      @controller_class_name = "#{@controller_class_nesting}::#{@controller_class_name_without_nesting}"
     end
   end
-
+ 
   def manifest
     record do |m|
       # Check for class naming collisions.
       m.class_collisions(controller_class_path, 
-	                     "#{controller_class_name}Controller", 
+	  					 "#{controller_class_name}Controller", 
 						 "#{controller_class_name}Helper")
 
-      m.class_collisions(class_path, "#{class_name}")
-
+      m.class_collisions(class_path, 
+	                     "#{class_name}")
+ 
       # Controller, helper, views, and test directories.
       m.directory(File.join('app/models',        class_path))
       m.directory(File.join('app/controllers',   controller_class_path))
@@ -81,9 +64,9 @@ class MyScaffoldGenerator < Rails::Generator::NamedBase
       m.directory(File.join('app/views/layouts', controller_class_path))
       m.directory(File.join('test/functional',   controller_class_path))
       m.directory(File.join('test/unit',         class_path))
-
+ 
       m.directory('public/stylesheets/blueprint')
-
+ 
       scaffold_views.each do |view|
         m.template("#{templating}/#{view}.html.#{templating}",
                    File.join('app/views', 
@@ -91,77 +74,80 @@ class MyScaffoldGenerator < Rails::Generator::NamedBase
 							 controller_file_name, 
 							 "#{view}.html.#{templating}"))
       end
-
+ 
       # Layout and stylesheet.
       m.template("#{templating}/layout.html.#{templating}", 
                  File.join('app/views/layouts', 
                            controller_class_path, 
                            "#{controller_file_name}.html.#{templating}"))
-
+ 
       %w(print 
 	     screen 
 		 ie).each do |stylesheet|
         m.template("blueprint/#{stylesheet}.css", 
 		           "public/stylesheets/blueprint/#{stylesheet}.css")
       end
-
+ 
       m.template('controller.rb', 
                  File.join('app/controllers', 
                             controller_class_path, 
                             "#{controller_file_name}_controller.rb"))
-
-      m.template("functional_test/basic.rb", 
+ 
+      m.template("functional_test/#{functional_test_style}.rb", 
                   File.join('test/functional', 
                             controller_class_path, 
                             "#{controller_file_name}_controller_test.rb"))
-
+ 
       m.template('helper.rb',          
                  File.join('app/helpers',     
                  controller_class_path, 
                  "#{controller_file_name}_helper.rb"))
-
+ 
       m.route_resources controller_file_name
-
+ 
       m.dependency 'my_model', [name] + @args, :collision => :skip
     end
   end
-
+ 
   def templating
-    options[:templating] || @configuration.templating
+    options[:templating] 
   end
-
+ 
+  def functional_test_style
+    options[:functional_test_style]
+  end
+ 
   protected
-  # Override with your own usage banner.
   def banner
-   "Usage: #{$0} scaffold ModelName [field:type, field:type]"
+    "Usage: #{$0} my_scaffold ModelName [field:type, field:type]"
   end
-
+ 
   def add_options!(opt)
     opt.separator ''
     opt.separator 'Options:'
+
     opt.on("--skip-timestamps",
            "Don't add timestamps to the migration file for this model") { |v| options[:skip_timestamps] = v }
-
+ 
     opt.on("--skip-migration",
            "Don't generate a migration file for this model") { |v| options[:skip_migration] = v }
-
-    opt.on("--templating [erb|haml]", 
-           "Specify the templating to use (haml by default)") { |v| options[:templating] = v }
-
-    opt.on("--functional-test-style [basic|should_be_restful]", 
+ 
+ 
+    opt.on("--functional-test-style [basic]", 
            "Specify the style of the functional test (should_be_restful by default)") { |v| options[:functional_test_style] = v }
       
    end
-
+ 
   def scaffold_views
     %w[ index 
         show 
-        new 
+    	new 
 		edit 
 		_form ]
   end
-
+ 
   def model_name
     class_name.demodulize
   end
 end
+
